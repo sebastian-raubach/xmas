56ea9ed0572221427e490208f28c93fe3e24fc7d
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2022-12-15 15:36:43 +0000

    - FIX: Vue relative path fix.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: false,
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  configureWebpack: {
+    // plugins: [
+    //   new BundleAnalyzerPlugin()
+    // ],
+    devtool: 'eval-source-map',
+    target: 'web'
+  }
 })
